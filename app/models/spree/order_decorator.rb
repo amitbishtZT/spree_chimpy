5ab19b8582bf7bd439cd8b807e6183ecef3d484c
root <root@DESKTOP-02O681N.localdomain>
2018-05-08 14:58:41 +0530

    updated order trnasition state

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -2,7 +2,7 @@ Spree::Order.class_eval do
   has_one :source, class_name: 'Spree::Chimpy::OrderSource'
 
   state_machine do
-    after_transition :to => :complete, :do => :notify_mail_chimp
+    after_transition :to => :confirm, :do => :notify_mail_chimp
   end
 
   around_save :handle_cancelation
